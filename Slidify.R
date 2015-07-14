12b3fbb06f7f3173b537398de2de78581831bb49
Lex Knape <lex@suitgeeks.com>
2015-07-14 17:32:26 +0200

    commit_1
    Create new read.md

---
+++ Slidify.R
@@ -0,0 +1,16 @@
+install.packages("stringr")
+library(stringr)
+
+
+
+install.packages("devtools")
+devtools::install_github("muschellij2/slidify")
+library(slidify)
+
+#install_github('slidify', 'ramnathv')
+#install_github('slidifyLibraries', 'ramnathv')
+
+setwd("/Users/anknape/Mainfolder/Study/Coursera/Data Science/Les 9 Data products/slidify")
+author("first test")
+
+
